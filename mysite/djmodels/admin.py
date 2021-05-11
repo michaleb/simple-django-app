1c2343e844f74e8f44c4e3f4aa391199e18a4442
michaleb <michale.bailey@gmail.com>
2021-05-11 15:46:27 -0400

    first model migration

--- mysite/djmodels/admin.py
+++ mysite/djmodels/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import School, Student, Faculty, Department, Grade, Certificate_type
 
-# Register your models here.
+admin.site.register(School)
+admin.site.register(Student)
+admin.site.register(Faculty)
+admin.site.register(Department)
+admin.site.register(Grade)
+admin.site.register(Certificate_type)
+
+
+
--- mysite/djmodels/models.py
+++ mysite/djmodels/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length =400)
+    
+class Faculty(models.Model):
+    name = models.CharField(max_length = 30)
+    faculty = models.ForeignKey(School, on_delete=models.CASCADE)
+    
+    
+class Department(models.Model):
+    department = models.CharField(max_length = 50)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    
+
+class Grade(models.Model):
+    type = models.DecimalField(decimal_places = 2, max_digits = 3)
+
+class Certificate_type(models.Model):
+    name = models.CharField(max_length = 30)
+
+class Student(models.Model):
+    
+    full_name = models.CharField(max_length = 50)
+    year_of_graduation = models.DateField(default =datetime.today().year)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    certificate = models.ForeignKey(Certificate_type, on_delete=models.CASCADE)
+
+    
+    
+    
+
+
